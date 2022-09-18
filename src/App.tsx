dd000c0d981589123efc7f9900a76533eb9feb5f
Raymon Shi <rshi.raymon@gmail.com>
2022-09-18 11:42:20 -0500

    changed the front page to a two tab design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import PokedexTabs from "./components/PokedexTabs";
 import PokedexSearch from "./components/PokedexSearch";
+import { Tabs, Tab } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
-      <PokedexSearch />
-      <PokedexTabs />
+      <Tabs defaultActiveKey="search" id="uncontrolled-tab" className="mb-3">
+        <Tab eventKey="search" title="Search">
+          <PokedexSearch />
+        </Tab>
+        <Tab eventKey="table" title="Table">
+          <PokedexTabs />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
