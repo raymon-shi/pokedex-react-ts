71fc04ab9e054a52fee62df5e4715074a87a41cf
Raymon Shi <rshi.raymon@gmail.com>
2022-09-19 19:49:32 -0500

    added pokemon search cards and based on string similarity

--- src/components/PokedexTable.tsx
+++ src/components/PokedexTable.tsx
@@ -1,57 +1,10 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import pokedex from "../data/pokedex.json";
-// import pokedexTest from "../data/pokedexTest.json";
+import { IPokemon } from "../interface";
 
 interface IProps {
   generationTitle: string | null;
-}
-
-interface IName {
-  english: string;
-  japanese: string;
-  chinese: string;
-  french: string;
-}
-
-interface IBaseStats {
-  HP?: number;
-  Attack?: number;
-  Defense?: number;
-  "Sp. Attack"?: number;
-  "Sp. Defense"?: number;
-  Speed?: number;
-}
-
-interface IEvolution {
-  prev?: string[] | undefined;
-  next?: string[][] | undefined;
-}
-
-interface IProfile {
-  height: string;
-  weight: string;
-  egg?: string[] | undefined;
-  ability?: string[][] | undefined;
-  gender: string;
-}
-
-interface IImage {
-  sprite: string;
-  thumbnail: string;
-  hires?: string;
-}
-
-interface IPokemon {
-  id: number;
-  name: IName;
-  type: string[];
-  base?: IBaseStats;
-  species: string;
-  description: string;
-  evolution?: IEvolution;
-  profile: IProfile;
-  image: IImage;
 }
 
 const gen1: IPokemon[] = pokedex.slice(0, 151);
