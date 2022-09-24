e0a8057b4935b0c12ba3a8ad6f992f5c295e8823
Raymon Shi <rshi.raymon@gmail.com>
2022-09-24 12:47:40 -0500

    changed how searching is done

--- src/components/PokedexSearch.tsx
+++ src/components/PokedexSearch.tsx
@@ -4,40 +4,31 @@ import pokedex from "../data/pokedex.json";
 import { IPokemon } from "../interface";
 import { compareTwoStrings } from "string-similarity";
 import PokedexSearchResults from "./PokedexSearchResults";
+import Select from "react-select";
+
+interface IOption {
+  value: string;
+  label: string;
+}
 
 const PokedexSearch = () => {
   const [pokemonName, setPokemonName] = useState<string>("");
   const [pokemonSearchResults, setPokemonSearchResults] = useState<IPokemon[]>([]);
   console.log(pokemonName);
 
-  const searchForPokemon = (pokemonSearchName: string):gitvoid => {
+  const searchForPokemon = (pokemonSearchName: string): void => {
     setPokemonSearchResults(
-      pokedex.filter((pokemon) => compareTwoStrings(pokemon.name.english.toLowerCase(), pokemonSearchName.toLowerCase()) >= 0.35),
+      pokedex.filter((pokemon) => compareTwoStrings(pokemon.name.english.toLowerCase(), pokemonSearchName.toLowerCase()) >= 0.5),
     );
   };
 
-  console.log(pokemonSearchResults);
-
   return (
     <Container>
-      <Form
-        style={{ textAlign: "left" }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          searchForPokemon(pokemonName);
-        }}>
+      <Form style={{ textAlign: "left" }}>
         <Form.Group className="mb-3" controlId="formBasicSearch">
-          <Form.Label>Enter the Pokemon</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter the Pokemon you want to search!"
-            value={pokemonName}
-            onChange={(e) => setPokemonName(e.target.value)}
-          />
+          <Form.Label>Enter the Pokemon name you want to search for</Form.Label>
+          <Form.Control type="text" placeholder="Enter the Pokemon you want to search!" onChange={(e) => searchForPokemon(e.target.value)} />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Search
-        </Button>
       </Form>
       <br />
       <PokedexSearchResults pokemonSearchResults={pokemonSearchResults} />
